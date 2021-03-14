0e75c1fc227199051056685d1c49f89697513f6b
LimpSquid <git@limpsquid.nl>
2021-03-14 23:16:55 +0100

    Removed camelcase

--- src/rest/net/routing/router.cpp
+++ src/rest/net/routing/router.cpp
@@ -18,11 +18,11 @@ router::~router()
 
 router_node &router::make_node(const std::string &expression)
 {
-    router_node newNode(expression);
+    router_node new_node(expression);
 
     for(const router_node &node : nodes_) {
-        if(node == newNode)
+        if(node == new_node)
             throw std::invalid_argument("Expression already specified for router");
     }
-    return nodes_.emplace_back(std::move(newNode));
+    return nodes_.emplace_back(std::move(new_node));
 }
