af63ea5ffef0c6718e1b0c7451a237691be23161
LimpSquid <git@limpsquid.nl>
2021-12-04 00:04:56 +0100

    Super ugly but we have a grid of LEDs. Mostly copied from the WWW, it is really time to start and understand graphics rendering.

--- src/cube/hal/mock/display.cpp
+++ src/cube/hal/mock/display.cpp
@@ -2,19 +2,34 @@
 #include <cube/hal/mock/window.h>
 #include <cube/core/color.h>
 #include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <math.h>
 
 using namespace cube::core;
 using namespace cube::hal::mock;
 
+#define SIZE 16
+
 namespace
 {
     color c;
+
+    int map[SIZE][SIZE][SIZE];
+
+    void cls(int col)
+    {
+    int x,y,z;
+    for (x=0;x<SIZE;x++)
+     for (y=0;y<SIZE;y++)
+      for (z=0;z<SIZE;z++)
+       map[x][y][z]=col;
+    }
 }
 
 display::display() :
     window_(window::instance())
 {
-
 }
 
 display::~display()
@@ -26,6 +41,9 @@ void display::show(graphics_buffer & buffer)
 {
     // Todo: temporary show color
     c = buffer.test_color;
+
+    int color = (int)c.b << 16 | (int)c.g << 8 | (int)c.r;
+    cls(color);
 }
 
 void display::poll()
@@ -35,15 +53,26 @@ void display::poll()
 
     window_.clear();
 
-    glBegin(GL_QUADS);
-    for (double i = 255.0; i < 510.0; i += 1.0) {
-        glColor3f(c.r / i, c.g / i, c.b / i);
-        glVertex2f(-c.r / i, -c.r / i);
-        glVertex2f(-c.g / i,  c.g / i);
-        glVertex2f( c.b / i,  c.b / i);
-        glVertex2f( c.b / i, -c.b / i);
-    }
+    int x, y, z;
+    float p[3],dp=1.0/float(SIZE-1);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE,GL_ONE);
+
+    glPointSize(3.0);
+    glBegin(GL_POINTS);
+
+    for (p[0]=-0.5,x=0;x<SIZE;x++,p[0]+=dp)
+     for (p[1]=-0.5,y=0;y<SIZE;y++,p[1]+=dp)
+      for (p[2]=-0.5,z=0;z<SIZE;z++,p[2]+=dp)
+        {
+            glColor4ubv((GLubyte*)(&map[x][y][z]));
+            glVertex3fv(p);
+        }
+
     glEnd();
+    glDisable(GL_BLEND);
+    glPointSize(1.0);
 
     window_.update();
 }
--- src/cube/hal/mock/window.cpp
+++ src/cube/hal/mock/window.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace cube::hal::mock;
 
@@ -12,6 +13,22 @@ namespace
     {
         std::cerr << "GLFW error: " << desc << "\n";
     }
+
+    struct V { GLdouble x, y, z; };
+
+    double norm(V v){ return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z); }
+
+    V operator-(V a, V b) { return {a.x - b.x, a.y - b.y, a.z - b.z}; }
+    V operator+(V a, V b) { return {a.x + b.x, a.y + b.y, a.z + b.z}; }
+    V operator*(V v, double s) { return {v.x * s, v.y * s, v.z * s}; }
+    V & operator+=(V & a, V b) { return a = a + b; }
+    V & operator-=(V & a, V b) { return a = a - b; }
+
+    V operator/(V v, double s) { return {v.x / s, v.y / s, v.z / s}; }
+    V normalize(V v){ return v / norm(v); }
+
+    V camera{0, 0, 2};
+    double rotation = 0;
 }
 
 window & window::instance(window_properties creation_properties)
@@ -34,6 +51,34 @@ void window::clear()
 
 void window::update()
 {
+    int width, height;
+    glfwGetFramebufferSize(glfw_window_, &width, &height);
+    float ratio = width / (float) height;
+    glViewport(0, 0, width, height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, ratio, 0.1, 30);
+
+    glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+    glTranslatef(-camera.x, -camera.y, -camera.z);
+
+    if (glfwGetKey(glfw_window_, GLFW_KEY_UP) == GLFW_PRESS)
+        camera.y += 0.01;
+    if (glfwGetKey(glfw_window_, GLFW_KEY_DOWN) == GLFW_PRESS)
+        camera.y -= 0.01;
+    if (glfwGetKey(glfw_window_, GLFW_KEY_LEFT) == GLFW_PRESS)
+        camera.x -= 0.01;
+    if (glfwGetKey(glfw_window_, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        camera.x += 0.01;
+    if (glfwGetKey(glfw_window_, GLFW_KEY_HOME) == GLFW_PRESS)
+        camera.z -= 0.01;
+    if (glfwGetKey(glfw_window_, GLFW_KEY_END) == GLFW_PRESS)
+        camera.z += 0.01;
+
     glfwPollEvents();
     glfwSwapBuffers(glfw_window_);
 }
@@ -58,6 +103,7 @@ void window::window_init(window_properties const & properties)
     glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_REFRESH_RATE, 50);
 
     glfw_window_ = glfwCreateWindow(properties.width, properties.height, properties.title, NULL, NULL);
     if (!glfw_window_)
