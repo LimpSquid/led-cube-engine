f09e2b5cb06f5d6b38b30f7ec59b9f0717d48a19
LimpSquid <git@limpsquid.nl>
2021-12-25 01:55:06 +0100

    Move property_value and related converters to separate header.

--- src/cube/gfx/configurable_animation.hpp
+++ src/cube/gfx/configurable_animation.hpp
@@ -1,89 +1,12 @@
 #pragma once
 
+#include <cube/gfx/property_value.hpp>
 #include <cube/core/animation.hpp>
-#include <cube/core/color.hpp>
-#include <chrono>
 #include <vector>
-#include <sstream>
 #include <unordered_map>
 
 namespace cube::gfx
 {
-
-template<typename T>
-struct property_value_converter
-{
-    static std::string convert(T const & value)
-    {
-        std::stringstream stream;
-        stream << value;
-        return stream.str();
-    }
-
-    static T convert(std::string const & value)
-    {
-        T result;
-        std::stringstream stream(value);
-        stream >> result;
-        return result;
-    }
-};
-
-template<>
-struct property_value_converter<std::string>
-{
-    static std::string convert(std::string const & value) { return value; }
-};
-
-template<class Rep, class Period>
-struct property_value_converter<std::chrono::duration<Rep, Period>>
-{
-    static std::string convert(std::chrono::duration<Rep, Period> const & value)
-    {
-        return property_value_converter<Rep>::convert(value.count());
-    }
-
-    static std::chrono::duration<Rep, Period> convert(std::string const & value)
-    {
-        auto const count = property_value_converter<Rep>::convert(value);
-        return std::chrono::duration<Rep, Period>(count);
-    }
-};
-
-template<>
-struct property_value_converter<core::color>
-{
-    static std::string convert(core::color const & value)
-    {
-        std::stringstream stream;
-        stream << value.r << value.g << value.b << value.a;
-        return stream.str();
-    }
-
-    static core::color convert(std::string const & value)
-    {
-        core::color_t r, g, b, a;
-        std::stringstream stream(value);
-        stream >> r >> g >> b >> a;
-        return {r, g, b, a};
-    }
-};
-
-struct property_value
-{
-    template<typename T>
-    property_value(T const & v) :
-        property(property_value_converter<T>::convert(v))
-    { }
-
-    std::string const property;
-};
-
-template<>
-struct property_value_converter<property_value>
-{
-    static std::string convert(property_value const & value) { return value.property; }
-};
 
 class configurable_animation :
     public core::animation
@@ -99,7 +22,7 @@ public:
     template<typename T>
     void write_property(property_label_type label, T value)
     {
-        properties_[label] = property_value_converter<T>::convert(value);
+        properties_[label] = property_value_converter<T>{}(value);
     }
 
     void write_properties(std::vector<std::pair<property_label_type, property_value>> const & properties);
@@ -113,7 +36,7 @@ public:
             return def;
 
         // Todo: maybe do some checking if we are able to convert the string to the given type.
-        return property_value_converter<T>::convert(search->second);
+        return property_value_converter<T>{}(search->second);
     }
 
 protected:
